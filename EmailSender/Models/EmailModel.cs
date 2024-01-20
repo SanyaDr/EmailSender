2b7393428b9002506b42ada60b6964a3d8d3228d
Alexander <mr.drovosekov2006@yandex.ru>
2024-01-21 00:19:15 +0500

    add new email page add EmailModel.cs

---
+++ EmailSender/Models/EmailModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EmailSender.Models;
+
+public class EmailModel
+{
+    [Required(ErrorMessage = "Введите email получателя!")]
+    public string Email { get; set; }
+    public string Subject { get; set; }
+    [Required(ErrorMessage = "Введите текст сообщения")]    
+    public string Text { get; set; }
+}
